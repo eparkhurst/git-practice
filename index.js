1dde2e82b6185960f9c871b9b30de801d1710039
eparkhurst <eli.parkhurst@gmail.com>
2016-07-14 15:24:26 -0600

    added quote from baby branch

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 //This is the begining
 //This the second thing that I did locally
+function firstBranch(){
+  console.log('firstBranch is the best branch.');
+  console.log("after baby branch");
+}
